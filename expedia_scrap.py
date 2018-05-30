eb527bae9a84a14f869dc923fb6bcee22f865392
anantupadhyay <anantupadhyay17@gmail.com>
2018-05-30 13:06:00 +0530

    Comments Added

--- expedia_scrap.py
+++ expedia_scrap.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from selenium import webdriver 
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait 
@@ -8,27 +9,34 @@ from bs4 import BeautifulSoup
 import time
 import codecs
 
+# GLOBAL VARIABLES THAT WILL STORE THE REVIEW, DATE OF REVIEW AND RATING
 review = []
 date = []
 rating = []
 
+# OPENING THE URL USING SELENIUM IN CHROME AND PASSING THE PAGE SOURCE TO SOUP
 def get_response_from_server(url):
 	try:
-		cnt = 0
+		# Defining the browser
 		browser = webdriver.Chrome()
 		browser.get(url)
+		# Clicking on element to load user review's
 		browser.find_element_by_xpath('.//*[@id="link-to-reviews"]').click()
+		# Waiting for the reviews to load
 		wait = WebDriverWait(browser, 13)
 
 		while(True):
-			
+			# Wait time after we click 'Next' button of review list every time
 			time.sleep(15)
-			cnt += 1
-			# store it to string variable
+			# Store page source into a string variable
 			page = browser.page_source
+			# Passing the page source to BeautifulSoup
 			soup=BeautifulSoup(page, 'html.parser')
+			# Calling the main function to extract the details of review
 			scrap_logic(soup)
 
+			# Logic to keep loading new reviews until no new reviews can be loaded
+			# Each block of reviews are processed and then next button is clicked
 			try:
 				element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'pagination-next')))
 				print element
@@ -42,14 +50,13 @@ def get_response_from_server(url):
 		return
 
 def scrap_logic(soup):
-	#soup = BeautifulSoup(open("expedia.html"), "html.parser")
-	flag = True
+	# Getting details from the page
 	detail = soup.find_all('div', {'class': 'details'})
 	dt = soup.find_all('div', {'class': 'date-posted'})
 	x = 0
 	for tag in detail:
-		#print "here"
 		rvtxt = tag.find('span', {'class': 'translate-text'})
+		# If review text is empty then, we do not fetch date and rating for that review
 		if rvtxt == None:
 			x += 1
 			continue
@@ -62,17 +69,18 @@ def scrap_logic(soup):
 		date.append(' '.join(word for word in tmp))
 		x += 1
 
-url = "https://www.expedia.co.in/Ooty-Hotels-Kurumba-Village-Resort.h6129303.Hotel-Information?chkin=29%2F05%2F2018&chkout=30%2F05%2F2018&rm1=a2&hwrqCacheKey=f7945c2a-d72b-462c-a6af-254594b327a2HWRQ1527593270029&cancellable=false&regionId=6234125&vip=false&c=a3c473ef-ac7b-400f-a1ab-82c2b0d7b8d0&&exp_dp=13409.93&exp_ts=1527593245227&exp_curr=INR&swpToggleOn=false&exp_pg=HSR"
-get_response_from_server(url)
+if __name__=="__main__" :
+	# URL of hotel for which details have to be fetched
+	url = "https://www.expedia.co.in/Ooty-Hotels-Kurumba-Village-Resort.h6129303.Hotel-Information?chkin=29%2F05%2F2018&chkout=30%2F05%2F2018&rm1=a2&hwrqCacheKey=f7945c2a-d72b-462c-a6af-254594b327a2HWRQ1527593270029&cancellable=false&regionId=6234125&vip=false&c=a3c473ef-ac7b-400f-a1ab-82c2b0d7b8d0&&exp_dp=13409.93&exp_ts=1527593245227&exp_curr=INR&swpToggleOn=false&exp_pg=HSR"
+	get_response_from_server(url)
 
-#scrap_logic("abc")
+	print ("Total reviews are")
+	print (len(review), len(date))
 
-print ("Total reviews are")
-print (len(review), len(date))
-
-with codecs.open("output.txt", "w", encoding="utf-8") as thefile:
-	for x in range(len(review)):
-		thefile.write("%s\n" % date[x].decode("utf-8"))
-		thefile.write("%s\n" % rating[x].decode("utf-8"))
-		thefile.write("%s\n\n" % review[x].decode("utf-8"))
-thefile.close()
+	# Writing results to a text file
+	with codecs.open("output.txt", "w", encoding="utf-8") as thefile:
+		for x in range(len(review)):
+			thefile.write("%s\n" % date[x].decode("utf-8"))
+			thefile.write("%s\n" % rating[x].decode("utf-8"))
+			thefile.write("%s\n\n" % review[x].decode("utf-8"))
+	thefile.close()
